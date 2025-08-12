fffbcb19befbcaf226e1b6cf8d1d0d73e2b9a2f1
Tom H <dev@quickbuzz.org>
2025-08-12 13:36:52 +0100

    Initial commit with proper .gitignore2

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "= 3.116.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "~> 3.4"
+      version = "= 3.6.0"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.0"
 
  /* backend "azurerm" {
     resource_group_name  = "rg-terraform-state"             
